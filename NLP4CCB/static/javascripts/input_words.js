764ae40043bdfa7dc7f13a02f37cea8d0aad695c
Ross Mechanic <ross.mechanic@icloud.com>
2017-01-30 20:25:14 -0500

    Timer works

--- NLP4CCB/static/javascripts/input_words.js
+++ NLP4CCB/static/javascripts/input_words.js
@@ -2,9 +2,19 @@
  * Created by rossmechanic on 11/29/16.
  */
 $(document).ready(function(){
-    $('.word-rel-formset').hover(function(){
-        $(this).children(".glyphicon").removeClass("hidden");
-    }, function(){
-        $(this).children(".glyphicon").addClass("hidden");
-    });
+    var $timer = $(".timer");
+    var time = parseInt($timer.text())
+    var timeUpdater = window.setInterval(function(){
+        time -= 1;
+        if (time === 0){
+            window.clearInterval(timeUpdater);
+        }
+    },1000);
+
+    var timerDOMUpdater = window.setInterval(function(){
+        $timer.text(time);
+        if (time === 0){
+            window.clearInterval(timerDOMUpdater);
+        }
+    },500);
 });
