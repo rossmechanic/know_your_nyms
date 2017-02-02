1abd92974f9b77aa01e7a39cfb480f5c2fa0a36e
Dean Fulgoni <dfulgoni@seas.upenn.edu>
2017-02-02 00:31:46 -0500

    Now can ask for either meronyms or hyponyms. Also matches words in case insensitive manner, instead of breaking or not matching

--- NLP4CCB/utils.py
+++ NLP4CCB/utils.py
@@ -6,8 +6,10 @@ from NLP4CCB_Django_App import settings
 def get_matched_pairs_scores(base_word, input_words, sem_rel):
 	json_f = 'data/wordnet_' + sem_rel + '.json'
 	# Now will get json data of known meronym pairs.
-	known_meronym_pairs = json.load(open(os.path.join(settings.STATIC_ROOT, json_f)))
+	known_pairs = json.load(open(os.path.join(settings.STATIC_ROOT, json_f)))
 	# Pairs for "person". Hardcoded for now. Should access the appropriate base word in the future.
-	base_word_meronyms = known_meronym_pairs[base_word]
-	pairs_and_scores = {word: 1 if word in base_word_meronyms else 0 for word in input_words if word}
+	base_word_pairs = known_pairs[base_word]
+	print(base_word_pairs)
+	pairs_and_scores = {word: 1 if str(word).lower() in map(str.lower, map(str, base_word_pairs)) else 0
+															for word in input_words if word}
 	return pairs_and_scores
--- NLP4CCB/views.py
+++ NLP4CCB/views.py
@@ -8,26 +8,32 @@ import random
 # Create your views here.
 def index(request):
 	word_relationship_formset = formset_factory(WordRelationshipForm, extra=1)
-	word_list = ['computer','fish','face','hand','person','dog']
-	selected_word = random.choice(word_list)
+	sem_rel = random.choice(['meronyms','hyponyms'])
+	if sem_rel == 'meronyms':
+		question = 'Name parts of a '
+	elif sem_rel == 'hyponyms':
+		question = 'Name kinds of a '
+	base_word = random.choice(['computer','fish','face','hand','person','dog'])
 	context = {
 		"title": "NLP4CCB",
 		"formset": word_relationship_formset,
-		"base_word": selected_word
+		"base_word": base_word,
+		"sem_rel": sem_rel,
+		"question": question
 	}
 	return render(request, 'input_words.html', context)
 
 def scoring(request):
 	# if this is a POST request, we need to process the form data
 	if request.method == 'POST':
+		sem_rel = request.POST['sem_rel']
 		base_word = request.POST['base_word']
 		num_forms_returned = int(request.POST['form-TOTAL_FORMS'])
 		input_words = [request.POST["form-%s-word" % i] for i in range(num_forms_returned)]
 		context = {}
 		# Creates scores based on the words and the semantic relationship
 		context['base_word'] = base_word
-		context['words_and_scores'] = utils.get_matched_pairs_scores(base_word, input_words, 'meronyms')
-
+		context['words_and_scores'] = utils.get_matched_pairs_scores(base_word, input_words, sem_rel)
 		print context
 		return render(request, 'scoring.html', context)
 	else:
