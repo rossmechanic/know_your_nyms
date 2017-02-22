5238032a062e333e1f0d8675f27f74a920f6ff4c
rossmechanic <ross.mechanic@icloud.com>
2017-02-22 00:24:24 -0500

    Integrated Hannah's homepage

---
+++ NLP4CCB/static/javascripts/welcome.js
@@ -0,0 +1,5 @@
+/**
+ * Created by rossmechanic on 2/21/17.
+ */
+$(document).ready(function(){
+});
--- NLP4CCB/urls.py
+++ NLP4CCB/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.models, name='index'),
     url(r'^scoring/', views.scoring, name='index')
 ]
