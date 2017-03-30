132072a70ab2ba78e15c6d2aabc642f70b953611
rossmechanic <ross.mechanic@icloud.com>
2017-03-30 00:10:55 -0400

    Added actions on challenges and syncing for relations

--- NLP4CCB/admin.py
+++ NLP4CCB/admin.py
@@ -1,18 +1,46 @@
 from django.contrib import admin
 from models import UserStat, Relation, UserInput, Challenge
 
+
+def accept_challenge(modeladmin, request, queryset):
+	for challenge in queryset:
+		rel_challenges = Challenge.objects.filter(relation=challenge.relation)
+		rel_challenges.update(is_pending=False)
+
+		challenge.relation.challenge_accepted = True
+		challenge.relation.save()
+
+accept_challenge.short_description = "Accept selected challenges"
+
+
+def deny_challenge(modeladmin, request, queryset):
+	for challenge in queryset:
+		rel_challenges = Challenge.objects.filter(relation=challenge.relation)
+		rel_challenges.update(is_pending=False)
+
+		challenge.relation.challenge_accepted = False
+		challenge.relation.save()
+
+deny_challenge.short_description = "Deny selected challenges"
+
+
 # Register your models here.
 class UserStatAdmin(admin.ModelAdmin):
-    list_display = ['user', 'rounds_played', 'total_score', 'index']
+	list_display = ['user', 'rounds_played', 'total_score', 'index']
+
 
 class RelationAdmin(admin.ModelAdmin):
-    list_display = ['type', 'base_word', 'input_word', 'in_word_net', 'challenge_accepted']
+	list_display = ['type', 'base_word', 'input_word', 'in_word_net', 'challenge_accepted']
+
 
 class UserInputAdmin(admin.ModelAdmin):
-    list_display = ['user', 'round_number', 'relation', 'word_score']
+	list_display = ['user', 'round_number', 'relation', 'word_score']
+
 
 class ChallengeAdmin(admin.ModelAdmin):
-    list_display = ['user', 'relation', 'sentence', 'is_pending']
+	list_display = ['user', 'relation', 'sentence', 'is_pending']
+	actions = [accept_challenge, deny_challenge]
+
 
 admin.site.register(UserStat, UserStatAdmin)
 admin.site.register(Relation, RelationAdmin)
---
+++ NLP4CCB/management/commands/sync_relations_with_wordnet.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand, CommandError
+from NLP4CCB_Django_App import settings
+import os
+import json
+from NLP4CCB.models import Relation
+
+class Command(BaseCommand):
+	help = 'Syncs current relations in the database such that the in_wordnet column is accurate'
+
+	def handle(self, *args, **options):
+		json_path = os.path.join(settings.STATIC_ROOT, 'data/wordnet_meronyms.json')
+		wordnet = json.load(open(json_path, 'r'))
+
+		for relation in Relation.objects.all():
+			try:
+				relation.in_word_net = relation.input_word in wordnet[relation.base_word]
+				relation.save()
+			except:
+				continue
