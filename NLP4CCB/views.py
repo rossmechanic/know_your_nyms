0320911f620e22e7321ca7e053bc20fa82f0e39f
rossmechanic <ross.mechanic@icloud.com>
2017-04-03 20:55:48 -0400

    Fixed homepage error

--- NLP4CCB/views.py
+++ NLP4CCB/views.py
@@ -17,14 +17,13 @@ vocab = [word.lower().strip() for word in lines]
 len_vocab = len(vocab)
 
 def index(request):
+	context = {}
 	try:
 		user_stat = UserStat.objects.get(user=request.user)
-	except ObjectDoesNotExist:
-		user_stat = UserStat.objects.create(user=request.user)
-		user_stat.save()
-	context = {}
-	context['rounds_played'] = user_stat.rounds_played
-	context['average_score'] = round(user_stat.total_score / user_stat.rounds_played,2)
+		context['rounds_played'] = user_stat.rounds_played
+		context['average_score'] = round(user_stat.total_score / user_stat.rounds_played,2)
+	except TypeError:
+		pass
 	return render(request, 'welcome.html', context)
 
 
