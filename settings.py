41b4e53e685c37759f69a345101834a2e04eb9f1
Ross Mechanic <ross.mechanic@icloud.com>
2019-04-18 18:13:36 -0400

    Fix static file issue

--- settings.py
+++ settings.py
@@ -122,5 +122,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "know_your_nyms/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = "/static/"
